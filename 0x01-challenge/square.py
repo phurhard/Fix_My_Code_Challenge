c89a7bdcb55b101de279af836737a70c065fdf5b
phurhard <phurhardeen@gmail.com>
2023-05-14 13:25:03 +0100

    square

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -7,32 +7,24 @@ class square():
 
 
     def __init__(self, *args, **kwargs):
+        """Iitialization"""
         for key, value in kwargs.items():
             setattr(self, key, value)
-'''
-    def validate(self, name, value):
-        """ Validation of the input values to the rectangle class"""
-        if type(value) != int:
-            raise TypeError(f'{name} must be an integer')
-        if name in ['width', 'height']:
-            if value <= 0:
-                raise ValueError(f'{name} must be > 0')
-            else:
-                return value
-'''
 
     def area_of_my_square(self):
         """ Area of the square """
         return self.width * self.height
 
     def PermiterOfMySquare(self):
+        """ Perimeter of the square"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """ String represetation"""
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
-
+    """ Creatig a square instance"""
     s = square(width=12, height=12)
     print(s)
     print(s.area_of_my_square())
